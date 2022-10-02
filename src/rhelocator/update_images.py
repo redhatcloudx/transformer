99cfa147245c6fa26458e954ce289566eeebd54a
Major Hayden <major@mhtx.net>
2022-10-02 18:48:10 -0500

    Add method for getting marketplace images

--- src/rhelocator/update_images.py
+++ src/rhelocator/update_images.py
@@ -13,3 +13,14 @@ def get_aws_regions() -> list[str]:
     ec2 = boto3.client("ec2", region_name="us-east-1")
     raw = ec2.describe_regions(AllRegions="True")
     return [x["RegionName"] for x in raw["Regions"]]
+
+
+def get_aws_marketplace_images(region: str) -> list[dict]:
+    """Get a list of RHEL marketplace images from an AWS region.
+
+    Args:
+        region: AWS region name, such as us-east-1
+    """
+    ec2 = boto3.client("ec2", region_name=region)
+    raw = ec2.describe_images(Owners=["679593333241"], IncludeDeprecated=False)
+    return raw["Images"]
--- tests/test_update_images.py
+++ tests/test_update_images.py
@@ -22,3 +22,23 @@ def test_get_aws_regions(mock_boto: any):
 
     assert isinstance(regions, list)
     assert "us-east-1" in regions
+
+
+@mock.patch("botocore.client.BaseClient._make_api_call")
+def test_get_aws_marketplace_images(mock_boto: any):
+    """Test AWS image request."""
+    mock_boto.return_value: dict = {
+        "Images": [
+            {
+                "ImageId": "ami-5731123e",
+            },
+        ],
+        "ResponseMetadata": {
+            "...": "...",
+        },
+    }
+
+    images = update_images.get_aws_marketplace_images("us-east-1")
+
+    assert isinstance(images, list)
+    assert images[0]["ImageId"] == "ami-5731123e"
