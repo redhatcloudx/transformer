85c8589b6d657c6386346d3109de0e420a908d47
Major Hayden <major@mhtx.net>
2022-10-02 20:32:12 -0500

    Remove old boto patch decorators

--- tests/test_update_images.py
+++ tests/test_update_images.py
@@ -1,24 +1,22 @@
 """Test image updates from remote cloud APIs."""
 from __future__ import annotations
 
-from unittest import mock
+from unittest.mock import patch
 
 from src.rhelocator import update_images
 
 
-@mock.patch("botocore.client.BaseClient._make_api_call")
-def test_get_aws_regions(mock_boto: any):
+def test_get_aws_regions():
     """Test AWS region request."""
-    with mock.patch("botocore.client.BaseClient._make_api_call") as boto:
+    with patch("botocore.client.BaseClient._make_api_call") as boto:
         update_images.get_aws_regions()
 
     boto.assert_called_with("DescribeRegions", {"AllRegions": "True"})
 
 
-@mock.patch("botocore.client.BaseClient._make_api_call")
-def test_get_aws_cloud_access_images(mock_boto: any):
+def test_get_aws_cloud_access_images():
     """Test AWS image request."""
-    with mock.patch("botocore.client.BaseClient._make_api_call") as boto:
+    with patch("botocore.client.BaseClient._make_api_call") as boto:
         update_images.get_aws_cloud_access_images("us-east-1")
 
     boto.assert_called_with(
