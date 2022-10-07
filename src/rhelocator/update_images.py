65f0a573b054ccc8ef75157e60ca2a3f2334059f
Janine Olear <pninak@web.de>
2022-10-07 17:25:41 +0200

    remove hardcoded project name
    Signed-off-by: Janine Olear <pninak@web.de>

--- src/rhelocator/update_images.py
+++ src/rhelocator/update_images.py
@@ -261,7 +261,7 @@ def get_google_images() -> list[str]:
     # NOTE(mhayden): Google's examples suggest using a filter here for "deprecated.state
     # != DEPRECATED" but it returns no images when I tried it.
     # https://github.com/googleapis/python-compute/blob/main/samples/recipes/images/pagination.py
-    images_list_request = compute_v1.ListImagesRequest(project="rhel-cloud")
+    images_list_request = compute_v1.ListImagesRequest(project=config.GCP_PROJECTNAME)
 
     return [
         x.name
