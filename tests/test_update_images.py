da2f2238e3b3a9f4a5d5ac92a68e1d0aaf1245e9
Major Hayden <major@redhat.com>
2022-10-10 13:36:09 -0500

    Add AWS image name parser
    Parse additional information about AWS images using their name.
    
    Fixes #34
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- tests/test_update_images.py
+++ tests/test_update_images.py
@@ -65,6 +65,82 @@ def test_get_aws_all_images(mock_aws_regions, mock_aws_images) -> None:
         assert billing_codes == {config.AWS_HOURLY_BILLING_CODE}
 
 
+def test_parse_aws_image_name_basic():
+    """Test parsing an AWS image name with a very basic image."""
+    image_name = "RHEL-7.9_HVM-20220512-x86_64-1-Hourly2-GP2"
+    data = update_images.parse_aws_image_name(image_name)
+
+    assert isinstance(data, dict)
+
+    assert not data["intprod"]
+    assert not data["extprod"]
+    assert data["version"] == "7.9"
+    assert data["virt"] == "HVM"
+    assert not data["beta"]
+    assert data["date"] == "20220512"
+    assert data["arch"] == "x86_64"
+    assert data["release"] == "1"
+    assert data["billing"] == "Hourly2"
+    assert data["storage"] == "GP2"
+
+
+def test_parse_aws_image_name_beta():
+    """Test parsing an AWS image name for a beta arm64 image."""
+    image_name = "RHEL-9.1.0_HVM_BETA-20220829-arm64-0-Hourly2-GP2"
+    data = update_images.parse_aws_image_name(image_name)
+
+    assert isinstance(data, dict)
+
+    assert not data["intprod"]
+    assert not data["extprod"]
+    assert data["version"] == "9.1.0"
+    assert data["virt"] == "HVM"
+    assert data["beta"] == "BETA"
+    assert data["date"] == "20220829"
+    assert data["arch"] == "arm64"
+    assert data["release"] == "0"
+    assert data["billing"] == "Hourly2"
+    assert data["storage"] == "GP2"
+
+
+def test_parse_aws_image_name_internal_product():
+    """Test parsing an AWS image name with an internal product."""
+    image_name = "RHEL_HA-8.5.0_HVM-20211103-x86_64-0-Hourly2-GP2"
+    data = update_images.parse_aws_image_name(image_name)
+
+    assert isinstance(data, dict)
+
+    assert data["intprod"] == "HA"
+    assert not data["extprod"]
+    assert data["version"] == "8.5.0"
+    assert data["virt"] == "HVM"
+    assert not data["beta"]
+    assert data["date"] == "20211103"
+    assert data["arch"] == "x86_64"
+    assert data["release"] == "0"
+    assert data["billing"] == "Hourly2"
+    assert data["storage"] == "GP2"
+
+
+def test_parse_aws_image_name_external_product():
+    """Test parsing an AWS image name for a beta arm64 image."""
+    image_name = "RHEL-SAP-8.2.0_HVM-20211007-x86_64-0-Hourly2-GP2"
+    data = update_images.parse_aws_image_name(image_name)
+
+    assert isinstance(data, dict)
+
+    assert not data["intprod"]
+    assert data["extprod"] == "SAP"
+    assert data["version"] == "8.2.0"
+    assert data["virt"] == "HVM"
+    assert not data["beta"]
+    assert data["date"] == "20211007"
+    assert data["arch"] == "x86_64"
+    assert data["release"] == "0"
+    assert data["billing"] == "Hourly2"
+    assert data["storage"] == "GP2"
+
+
 @patch("rhelocator.update_images.requests.post")
 def test_get_azure_access_token(mock_post) -> None:
     """Test retrieving Azure locations."""
