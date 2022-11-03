fa0d0ebd8e0bd8f6bb418009952d6ce20dcfc430
Major Hayden <major@redhat.com>
2022-11-03 10:59:21 -0500

    Ignore mypy error for lambda
    Also added a docstring for the validation function. 😉
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- src/rhelocator/update_images/schema.py
+++ src/rhelocator/update_images/schema.py
@@ -93,9 +93,20 @@ SCHEMA = {
 
 
 def validate_json(data: str) -> None:
+    """Validate a JSON document against the schema.
+
+    Args:
+        data: JSON string to validate.
+
+    Returns:
+        None if the schema is valid, or raises a ValidationError if it is invalid.
+    """
     try:
         validator = Draft202012Validator(SCHEMA)
-        errors = sorted(validator.iter_errors(data), key=lambda e: e.path)
+        errors = sorted(
+            validator.iter_errors(data),
+            key=lambda e: e.path,  # type: ignore[no-any-return]
+        )
 
         if errors:
             error_message = "Error validating image data: "
