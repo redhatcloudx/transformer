3b2dafb5ae28db0095d2116da85ef883c838753c
Janine Olear <pninak@web.de>
2022-11-23 18:27:17 +0100

    wip
    Signed-off-by: Janine Olear <pninak@web.de>

--- tests/update_images/testdata/update.sh
+++ tests/update_images/testdata/update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-az vm image list -f RHEL -l eastus --all > azure_list_images.json
+#az vm image list -f RHEL -l eastus --all > azure_list_images.json
+#gcloud auth application-default login
+gcloud config set account rhel-cloud
 gcloud compute images list --format="json" > gcp_list_images.json
 aws --region=us-east-1 ec2 describe-images --owner 309956199498 --filter "Name=is-public,Values=true" > aws_list_images.json
