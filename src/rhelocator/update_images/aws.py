c1cdaee441faba7c16912df67d22e0b13620dc8d
fkolwa <fkolwa@redhat.com>
2022-12-15 08:19:52 +0100

    Add external product to AWS, Google image names

--- src/rhelocator/update_images/aws.py
+++ src/rhelocator/update_images/aws.py
@@ -160,8 +160,11 @@ def format_image(image: dict[str, str], region: str) -> dict[str, str]:
     version = additional_information["version"]
     beta = additional_information["beta"]
     billing = additional_information["billing"]
+    extprod = additional_information["extprod"]
+    intprod = additional_information["intprod"]
+    name_parts = ['RHEL', version, intprod, extprod, virt_type, arch, billing, beta]
 
-    name = f"RHEL {version} {virt_type} {arch} {billing} {beta}"
+    name = ' '.join([x for x in name_parts if x != ''])
     selflink = (
         f"https://console.aws.amazon.com/ec2/home?region={region}#launchAmi={image_id}"
     )
@@ -176,3 +179,4 @@ def format_image(image: dict[str, str], region: str) -> dict[str, str]:
         "selflink": selflink,
         "region": region,
     }
+
