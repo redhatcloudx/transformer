2602f2948f73ea85226f65705c719815146e498b
Janine Olear <pninak@web.de>
2023-02-13 12:43:20 +0100

    remove live tests
    Signed-off-by: Janine Olear <pninak@web.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,33 +19,6 @@ def api_server(runner):
     runner.invoke(cli.serve, ["--file-path=tests/api/testdata/formatted_images.json"])
 
 
-@pytest.mark.e2e
-def test_aws_hourly_images_live_opt_in_region(runner):
-    """Run a live test against the AWS API to get hourly images for opt-in
-    regions.
-
-    Further reading:
-    https://github.com/redhatcloudx/cloudimagedirectory/issues/43
-    """
-    result = runner.invoke(cli.aws_hourly_images, ["--region=af-south-1"])
-    parsed = json.loads(result.output)
-
-    # Ensure we only received hourly images with the proper billing code.
-    assert {"hourly" in x["name"] for x in parsed["images"]["aws"]}
-    assert result.exit_code == 0
-
-
-@pytest.mark.e2e
-def test_aws_hourly_images_live(runner):
-    """Run a live test against the AWS API to get hourly images via CLI."""
-    result = runner.invoke(cli.aws_hourly_images, ["--region=us-east-1"])
-    parsed = json.loads(result.output)
-
-    # Ensure we only received hourly images with the proper billing code.
-    assert {"hourly" in x["name"] for x in parsed["images"]["aws"]}
-    assert result.exit_code == 0
-
-
 def test_aws_hourly_images_offline(runner, mock_aws_regions, mock_aws_images):
     """Run an offline test against the AWS API to get hourly images via CLI."""
     result = runner.invoke(cli.aws_hourly_images, ["--region=us-east-1"])
@@ -65,17 +38,6 @@ def test_aws_hourly_images_invalid_region(mock_aws_regions, runner):
     assert result.exit_code == 2
 
 
-@pytest.mark.e2e
-def test_aws_regions_live(runner):
-    """Run a live test against the AWS API to get hourly images via CLI."""
-    result = runner.invoke(cli.aws_regions)
-    parsed = json.loads(result.output)
-
-    assert isinstance(parsed, list)
-    assert "us-east-1" in parsed
-    assert result.exit_code == 0
-
-
 def test_aws_regions_offline(mock_aws_regions, runner):
     """Run an offline test against the AWS API to get hourly images via CLI."""
     result = runner.invoke(cli.aws_regions)
@@ -83,28 +45,6 @@ def test_aws_regions_offline(mock_aws_regions, runner):
 
     assert isinstance(parsed, list)
     assert "us-east-1" in parsed
-    assert result.exit_code == 0
-
-
-@pytest.mark.e2e
-def test_azure_images_live(runner):
-    """Run a live test against the Azure API to get images via CLI."""
-    result = runner.invoke(cli.azure_images)
-    parsed = json.loads(result.output)
-
-    assert isinstance(parsed["images"]["azure"], list)
-
-    for image in parsed["images"]["azure"]:
-        expected_keys = [
-            "name",
-            "arch",
-            "version",
-            "imageId",
-            "date",
-            "virt",
-        ]
-        assert list(image.keys()) == expected_keys
-
     assert result.exit_code == 0
 
 
@@ -129,21 +69,6 @@ def test_azure_images_offline(
             "date",
             "virt",
         ]
-        assert list(image.keys()) == expected_keys
-
-    assert result.exit_code == 0
-
-
-@pytest.mark.e2e
-def test_google_images_live(runner):
-    """Run a live test against the Google Cloud API to get images via CLI."""
-    result = runner.invoke(cli.google_images)
-    parsed = json.loads(result.output)
-
-    assert isinstance(parsed["images"]["google"], list)
-
-    for image in parsed["images"]["google"]:
-        expected_keys = ["name", "arch", "version", "imageId", "date", "selflink"]
         assert list(image.keys()) == expected_keys
 
     assert result.exit_code == 0
