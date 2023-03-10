17fdf69b61c3087c910901ad425a675d4ffd02b7
Janine Olear <pninak@web.de>
2023-03-10 12:49:54 +0100

    add alternative connection types
    Signed-off-by: Janine Olear <pninak@web.de>

--- src/cloudimagedirectory/transformer.py
+++ src/cloudimagedirectory/transformer.py
@@ -7,11 +7,24 @@ from cloudimagedirectory.transform import transform
 @click.command()
 @click.option('-id', '--origin.s3.aws.key-id', 'id', prompt='key-id', help='The key-id from your s3 bucket')
 @click.option('-key', '--origin.s3.aws.access-key', 'access_key', prompt='Your authentication key', help='The authentication key for the s3 bucket')
-@click.option('-op', '--origin.s3.path', 'origin_path', prompt='Origin s3 bucket path', help='The path to the source s3 bucket')
-@click.option('-dp', '--destination.s3.path', 'destination_path', prompt='Destination s3 bucket path', help='The path to the destination s3 bucket')
+@click.option('-op', '--origin.path', 'origin_path', prompt='Origin bucket path', help='The path to the source s3 bucket')
+@click.option('-dp', '--destination.path', 'destination_path', prompt='Destination bucket path', help='The path to the destination s3 bucket')
 @click.option('-b', '--origin.s3.bucket', 'bucket', prompt='Bucket name', help='The bucket name in the s3 bucket')
 @click.option('-v', '--image.api.version', 'api', prompt='api-version', help='Image api version')
-def run(id: str, access_key: int, origin_path: str, destination_path: str, bucket: str, api: str) -> None:
+@click.option('-f', '--input.files', 'arg_files', default="None", prompt='files to process', help='List of predefined files to process')
+def run(id: str, access_key: int, origin_path: str, destination_path: str, bucket: str, api: str, arg_files: str) -> None:
+    """
+    target = []
+    if arg_files != "None":
+        target = arg_files.split(",")
+    origin_connection = s3.ConnectionFS(origin_path, target)
+    origin_connection.put_content(s3.DataEntry("test.txt", "hallo-welt"))
+    data = origin_connection.get_content(s3.DataEntry("test.txt", ""))
+    print(data.content)
+    filenames = origin_connection.get_filenames()
+    for file in filenames:
+        print(file.filename)
+    """
     """Connect to s3 bucket and format image data."""
     origin_connection = s3.ConnectionAWS(id, access_key, origin_path, bucket)
     try:
