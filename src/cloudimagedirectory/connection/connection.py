afa7caf22ab43435323551e5a29ec8a41ddc299e
Janine Olear <pninak@web.de>
2023-03-13 13:23:30 +0100

    rely on fs connection with s3cmd
    Signed-off-by: Janine Olear <pninak@web.de>

---
+++ src/cloudimagedirectory/connection/connection.py
@@ -0,0 +1,76 @@
+import boto3
+import json
+from pathlib import Path
+import pathlib
+import os
+
+exception_not_connected = Exception("Client is not connected to s3 bucket")
+exception_already_connected = Exception("Client is already connected to s3 bucket")
+exception_path_not_existing = Exception("The given path to the filesystem doesn't exist")
+exception_not_implemented = Exception("Not implemented")
+
+class Connection:
+    def connect(self):
+        pass
+
+    def get_filenames(self):
+        pass
+
+    def get_content(self, filename):
+        pass
+
+    def put_content(self, content, filename):
+        pass
+
+class DataEntry:
+    filename = ""
+    content = None
+
+    def __init__(self, filename, content):
+        self.filename = filename
+        self.content = content
+
+    def to_bytes(self):
+        return ' '.join(format(x, 'b') for x in bytearray(json.dumps(self.content), 'utf-8'))
+
+class ConnectionFS(Connection):
+    origin_path = ""
+    arg_files = []
+
+    def __init__(self, origin_path, arg_files):
+        self.arg_files = arg_files
+        self.origin_path = origin_path
+        if self.origin_path == "":
+            self.origin_path = os.getcwd()
+
+    def connect(self):
+        pass
+
+    def get_filenames(self) -> list[DataEntry]:
+        if len(self.arg_files) != 0:
+            result = []
+            for file in self.arg_files:
+                result.append(DataEntry(file, None))
+            return result
+        print("suche")
+        return self.__list_files(self.origin_path)
+
+    def __list_files(self, dir:str) -> list[DataEntry]:
+        data_files = []
+        p = pathlib.Path(dir)
+        if p.exists():
+            for child in p.glob('**/*.json'):
+                data_files.append(DataEntry(str(child.resolve()), None))
+        else:
+            raise exception_path_not_existing
+        return data_files
+
+    def get_content(self, data) -> DataEntry:
+        content:dict = None
+        content = Path(data.filename).read_text()
+        content = json.loads(content)
+        return DataEntry(data.filename, content)
+
+    def put_content(self, data): # todo consider dst path
+        json_data = json.dumps(data.content)
+        Path(data.filename).write_text(json_data)
--- src/cloudimagedirectory/transformer.py
+++ src/cloudimagedirectory/transformer.py
@@ -1,43 +1,30 @@
 import click
 import sys
 
-from cloudimagedirectory.s3 import s3
+from cloudimagedirectory.connection import connection
 from cloudimagedirectory.transform import transform
 
 @click.command()
-@click.option('-id', '--origin.s3.aws.key-id', 'id', prompt='key-id', help='The key-id from your s3 bucket')
-@click.option('-key', '--origin.s3.aws.access-key', 'access_key', prompt='Your authentication key', help='The authentication key for the s3 bucket')
 @click.option('-op', '--origin.path', 'origin_path', prompt='Origin bucket path', help='The path to the source s3 bucket')
 @click.option('-dp', '--destination.path', 'destination_path', prompt='Destination bucket path', help='The path to the destination s3 bucket')
-@click.option('-b', '--origin.s3.bucket', 'bucket', prompt='Bucket name', help='The bucket name in the s3 bucket')
 @click.option('-v', '--image.api.version', 'api', prompt='api-version', help='Image api version')
 @click.option('-f', '--input.files', 'arg_files', default="None", prompt='files to process', help='List of predefined files to process')
-def run(id: str, access_key: int, origin_path: str, destination_path: str, bucket: str, api: str, arg_files: str) -> None:
-    """
+def run(origin_path: str, destination_path: str, api: str, arg_files: str) -> None:
+    """Get conentent from filesystem format image data."""
     target = []
     if arg_files != "None":
         target = arg_files.split(",")
-    origin_connection = s3.ConnectionFS(origin_path, target)
-    origin_connection.put_content(s3.DataEntry("test.txt", "hallo-welt"))
-    data = origin_connection.get_content(s3.DataEntry("test.txt", ""))
+    origin_connection = connection.ConnectionFS(origin_path, target)
+    origin_connection.put_content(connection.DataEntry("test.txt", "hallo-welt"))
+    data = origin_connection.get_content(connection.DataEntry("test.txt", ""))
     print(data.content)
     filenames = origin_connection.get_filenames()
     for file in filenames:
         print(file.filename)
-    """
-    """Connect to s3 bucket and format image data."""
-    origin_connection = s3.ConnectionAWS(id, access_key, origin_path, bucket)
-    try:
-        print("connect to s3 bucket: " + bucket)
-        origin_connection.connect()
-    except:
-        print("The connection to the s3 bucket didn't work")
-    print("fetch filenames: " + bucket)
-    files = origin_connection.get_filenames()
     
     pipeline = transform.Pipeline(origin_connection, [transform.TransformerAWS, transform.TransformerAZURE, transform.TransformerGOOGLE])
     print("run pipeline")
-    results = pipeline.run(files)
+    results = pipeline.run(filenames)
 
     print("upload results")
     version_prefix = ""
@@ -45,5 +32,5 @@ def run(id: str, access_key: int, origin_path: str, destination_path: str, bucke
         version_prefix = api + "/"
     for result in results:
         result.filename = destination_path + "/" + version_prefix + result.filename
-        print("put content to s3 bucket: " + bucket + " filename: " + result.filename)
-        origin_connection.put_content(result)
+        print("put content to filesystem. filename: " + result.filename)
+        #origin_connection.put_content(result)
