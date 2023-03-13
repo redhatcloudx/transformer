8fc259f373c73860fd869f3cb6504d9586809af2
Janine Olear <pninak@web.de>
2023-03-13 16:45:30 +0100

    add aws transformer
    Signed-off-by: Janine Olear <pninak@web.de>

--- src/cloudimagedirectory/transform/transform.py
+++ src/cloudimagedirectory/transform/transform.py
@@ -1,4 +1,6 @@
 from cloudimagedirectory.connection import connection
+from cloudimagedirectory.update_images import aws
+from cloudimagedirectory import config
 
 class Pipeline:
     transformer = []
@@ -36,15 +38,18 @@ class TransformerAWS(Transformer):
 
         results = []
         for e in entries:
-            raw_content = self.src_conn.get_content(e)
-            print("content")
-            print(raw_content.filename)
-            print(raw_content.content)
-            pass
-            # TODO: transform content in schema
-            # TODO: create data entries for schema files and return them
+            raw = self.src_conn.get_content(e)
+            region = raw.filename.split("/")
+            region = region[len(region)-1]
+            region = region.split(".")[0]
+            for content in raw.content:
+                if content["OwnerId"] != config.AWS_RHEL_OWNER_ID:
+                    continue
+                r = aws.format_image(content, region)
+                de = connection.DataEntry("aws/" + region + "/" + r["name"].replace(" ", "_").lower(), content)
+                results.append(de)
 
-        return [connection.DataEntry("aws/eu-west1/rhel-test.json", "test-content")]
+        return results
 
 class TransformerGOOGLE(Transformer):
     def run(self, data):
