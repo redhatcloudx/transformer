7f9af0bb985eed1777a561255b989fc8f5e6c0cf
Janine Olear <pninak@web.de>
2023-03-16 11:04:06 +0100

    cleanup
    Signed-off-by: Janine Olear <pninak@web.de>

--- tests/transformer/aws_test.py
+++ tests/transformer/aws_test.py
@@ -1,5 +1,13 @@
 import os
 
+
 def test_transformer_aws():
-    assert 0 == os.system('poetry run cloudimagedirectory-transformer -f ${PWD}/tests/transformer/testdata/input/aws/af-south-1.json -op=${PWD}  -dp=${PWD}/tests/transformer/testdata -v output')
-    assert 0 == os.system('diff ${PWD}/tests/transformer/testdata/expected/aws/af-south-1/rhel_6.10_hvm_x86_64_hourly2 ${PWD}/tests/transformer/testdata/output/aws/af-south-1/rhel_6.10_hvm_x86_64_hourly2')
+    assert 0 == os.system(
+        "poetry run cloudimagedirectory-transformer -f"
+        " ${PWD}/tests/transformer/testdata/input/aws/af-south-1.json -op=${PWD} "
+        " -dp=${PWD}/tests/transformer/testdata -v output"
+    )
+    assert 0 == os.system(
+        "diff ${PWD}/tests/transformer/testdata/expected/aws/af-south-1/rhel_6.10_hvm_x86_64_hourly2"
+        " ${PWD}/tests/transformer/testdata/output/aws/af-south-1/rhel_6.10_hvm_x86_64_hourly2"
+    )
