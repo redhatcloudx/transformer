08548bdffdb4e1ad41121dec23f7836a48ad964b
Major Hayden <major@redhat.com>
2023-03-24 09:07:27 -0500

    tests: use CLIRunner for testing
    Using click's built in CLIRunner gives us more control over how the
    tests are run. Switch to a temporary directory for output to avoid
    trampling over files in a user's directory.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- tests/transformer/google_test.py
+++ tests/transformer/google_test.py
@@ -1,14 +1,31 @@
+"""Tests for the Google transformer."""
 import os
 
+from cloudimagedirectory import transformer
 
-def test_transformer_google():
-    """Run transformer end to end with google input data."""
-    assert 0 == os.system(
-        "poetry run cloudimagedirectory-transformer -f"
-        " ${PWD}/tests/transformer/testdata/input/raw/google/all.json -op=${PWD} "
-        " -dp=${PWD}/tests/transformer/testdata -v output"
+
+def test_google_transformer_command(runner, tmp_path):
+    """Verify that we can transform Google data."""
+    runner.invoke(
+        transformer.run,
+        [
+            "-f",
+            "tests/transformer/testdata/input/raw/google/all.json",
+            "-op=.",
+            f"-dp={tmp_path}",
+            "-v",
+            "output",
+        ],
     )
-    assert 0 == os.system(
-        "diff ${PWD}/tests/transformer/testdata/expected/google/global/rhel_7_x86_64"
-        " ${PWD}/tests/transformer/testdata/output/google/global/rhel_7_x86_64"
-    )
+
+    # Ensure directories are made.
+    assert os.path.isdir(f"{tmp_path}/output/idx/list/sort-by-date")
+    assert os.path.isdir(f"{tmp_path}/output/google/global")
+
+    # Check list by date.
+    with open(f"{tmp_path}/output/idx/list/sort-by-date/0") as fileh:
+        assert "RHEL 7 X86_64" in fileh.read()
+
+    # Check image data.
+    with open(f"{tmp_path}/output/google/global/rhel_7_x86_64") as fileh:
+        assert "RHEL 7 X86_64" in fileh.read()
