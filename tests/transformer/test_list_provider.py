e274b30c432d49c9e47ad01067e4b0b99ee77f1a
Janine Olear <pninak@web.de>
2023-07-25 12:37:29 +0200

    add tests for TransformerV2ListProviderByOS
    Signed-off-by: Janine Olear <pninak@web.de>

---
+++ tests/transformer/test_list_provider.py
@@ -0,0 +1,86 @@
+"""Test for the list provider endpoint."""
+import filecmp
+import os
+
+from cloudimagedirectory import transformer
+
+
+def test_V2ListProvider(runner, tmp_path):
+    """Run transformer end to end and generate a list with all available
+    providers for the specific os."""
+    result = runner.invoke(
+        transformer.run,
+        [
+            "-f",
+            "tests/transformer/testdata/input/raw/google/all.json,tests/transformer/testdata/input/raw/aws/af-south-1.json,tests/transformer/testdata/input/raw/azure/eastus.json",
+            "-op=.",
+            f"-dp={tmp_path}",
+            "--filter.until=none",
+        ],
+    )
+
+    assert result.exit_code == 0, f"expected no error, but got code {result.exit_code} and output:\n{result.output}"
+
+    # Get current directory
+    pwd = os.getcwd()
+
+    # Check image data by comparing the expected file and the output file byte by byte.
+    assert filecmp.cmp(
+        f"{pwd}/tests/transformer/testdata/expected/v2/os/rhel/provider/list",
+        f"{tmp_path}/v2/os/rhel/provider/list",
+    )
+
+
+def test_transformerV2ListProvider(tmpdir):
+    """Test list provider run method."""
+    temporary_directory = tmpdir.mkdir("test")
+    connection = transformer.connection.ConnectionFS(temporary_directory, [])
+    runner = transformer.transform.TransformerV2ListProviderByOS(connection)
+    chunk_size = 2
+    runner.chunk_size = chunk_size
+    data = [
+        transformer.connection.DataEntry(
+            "v2/os/rhel/provider/azure/version/8/region/global/image/dba7673010f19a94af4345453005933fd511bea9",
+            {
+                "date": "2019-01-01",
+                "name": "test1",
+                "arch": "arch1",
+                "region": "global",
+            },
+        ),
+        transformer.connection.DataEntry(
+            "v2/os/rhel/provider/google/version/9/region/global/image/9054fbe0b622c638224d50d20824d2ff6782e308",
+            {
+                "date": "2023-03-06T12:57:17.827-08:00",
+                "name": "test2",
+                "arch": "ARM64",
+                "region": "global",
+            },
+        ),
+        transformer.connection.DataEntry(
+            "v2/os/rhel/provider/aws/version/8/region/ap-south-2/image/9054fbe0b622c638224d50d20824d2ff6782e308",
+            {
+                "date": "2020-01-01",
+                "name": "test2",
+                "arch": "arch2",
+                "region": "ap-south-2",
+            },
+        ),
+        transformer.connection.DataEntry(
+            "v2/os/unkown/provider/aws/version/7/region/some-region-1/image/9054fbe0b622c638224d50d20824d2ff6782e308",
+            {
+                "date": "2020-01-01",
+                "name": "test2",
+                "arch": "arch2",
+                "region": "region-1",
+            },
+        ),
+    ]
+    results = runner.run(data)
+    expected = transformer.connection.DataEntry(
+        "v2/os/rhel/provider/list",
+        {"aws": 1, "azure": 1, "google": 1},
+    )
+
+    assert expected.filename == results[0].filename
+    assert expected.content == results[0].content
